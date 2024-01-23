0d2578208c1c3bebb7a0a9ebb6d9d907d581fc9c
jm72458 <114184468+jm72458@users.noreply.github.com>
2024-01-22 21:18:44 -0500

    Main Page - Game information

--- menu.js
+++ menu.js
@@ -1,7 +1,11 @@
 const main_menu = document.getElementById("main_menu");
 const game_select_screen = document.getElementById("game_select_screen");
+const info_screen = document.getElementById("info_screen");
 const pong = document.getElementById("pong");
 const tetris = document.getElementById("tetris");
+const info = document.getElementById("info");
+const info_exit = document.getElementById("info_exit");
+
 
 pong.onclick = function() {
     window.location.href = "https://jm72458.github.io/Pong/index.html"
@@ -9,4 +13,14 @@ pong.onclick = function() {
 
 tetris.onclick = function() {
     window.location.href = "https://jm72458.github.io/Tetris/index.html"
+}
+
+info.onclick = function() {
+    game_select_screen.style.display = "none";
+    info_screen.style.display = "block";
+}
+
+info_exit.onclick = function() {
+    game_select_screen.style.display = "block";
+    info_screen.style.display = "none";
 }
