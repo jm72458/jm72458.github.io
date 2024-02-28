85e3434a7fece7ec050f5562c10a9888748957f2
jm72458 <114184468+jm72458@users.noreply.github.com>
2024-02-27 20:20:18 -0500

    Add files via upload

---
+++ Google/js/extrafeatures.js
@@ -0,0 +1,33 @@
+//Change url without refreshing page
+/*function ChangeUrl(page, url) {
+   if (typeof (history.pushState) != "undefined") {
+       var obj = { Page: page, Url: url };
+       history.pushState(obj, obj.Page, obj.Url);
+   } else {
+       alert("Browser does not support HTML5.");
+   }
+}
+
+$(function () {
+   $("urlChange").click(function () {
+       ChangeUrl('Page1', 'Page1.htm');
+})});*/
+
+
+var searches = ["How to", "What do", "Where do"];
+var num = 0;
+/*
+        
+        if (autoNum == 0) {
+          form.style["-webkit-box-shadow"] = "none !important";
+          form.style["-moz-box-shadow"] = "none !important";
+          form.style["box-shadow"] = "none !important";
+          //document.getElementById("searchbarautocomplete-list").style["border-color"] = "none";
+        }
+        else {
+          form.style["-webkit-box-shadow"] = "0px 1px 7px 0.5px rgba(0,0,0,.15) !important";
+          form.style["-moz-box-shadow"] = "0px 1px 7px 0.5px rgba(0,0,0,.15) !important";
+          form.style["box-shadow"] = "0px 1px 7px 0.5px rgba(0,0,0,.15) !important";
+          //document.getElementById("searchbarautocomplete-list").style["border-color"] = "black";
+        }
+*/
