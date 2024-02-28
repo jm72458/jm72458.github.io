b0ee73451fe65449aee385259cc6b36f14fa9fff
jm72458 <114184468+jm72458@users.noreply.github.com>
2024-02-27 20:22:19 -0500

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,11 +1,32 @@
 const main_menu = document.getElementById("main_menu");
+const skill_set_screen = document.getElementById("skill_set_screen");
+const html_css_screen = document.getElementById("html_css_screen");
 const game_select_screen = document.getElementById("game_select_screen");
 const info_screen = document.getElementById("info_screen");
+
+const html_css = document.getElementById("html_css");
+const javascript = document.getElementById("javascript");
+
+const google = document.getElementById("google");
+
 const pong = document.getElementById("pong");
 const tetris = document.getElementById("tetris");
 const info = document.getElementById("info");
 const info_exit = document.getElementById("info_exit");
 
+html_css.onclick = function() {
+    skill_set_screen.style.display = "none";
+    html_css_screen.style.display = "block";
+}
+
+javascript.onclick = function() {
+    skill_set_screen.style.display = "none";
+    game_select_screen.style.display = "block";
+}
+
+google.onclick = function() {
+    window.location.href = "https://jm72458.github.io/Google/index.html"
+}
 
 pong.onclick = function() {
     window.location.href = "https://jm72458.github.io/Pong/index.html"
