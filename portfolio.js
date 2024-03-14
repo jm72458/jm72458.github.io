c53ae01c670dcdc2ce728164b7864833f5225c25
jm72458 <114184468+jm72458@users.noreply.github.com>
2024-03-13 21:48:59 -0400

    Landing Page Redesign

---
+++ portfolio.js
@@ -0,0 +1,21 @@
+/*Highlight nav section based on where the user has scrolled on the page*/
+const sections = document.getElementsByTagName("section");
+
+window.addEventListener("scroll", navHighlight);
+
+function navHighlight() {
+  
+  let scrollY = window.scrollY;
+  
+    for (i = 0; i < sections.length; i++) {
+        const sectionHeight = sections[i].clientHeight;
+        const sectionTop = sections[i].offsetTop - 100;
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.getElementsByClassName("navItem")[i].classList.add("activeSection");
+        }
+        else {
+            document.getElementsByClassName("navItem")[i].classList.remove("activeSection");
+        }
+    }
+}
